67d50dae5592ebc28a6d25fbcb3d3bdf847c69ca
kimwoohyuck <dngur2961@naver.com>
2022-06-17 11:53:12 +0900

    CartDto 내용 채움

--- Pavetheway/src/main/java/com/pavetheway/myapp/cart/dto/CartDto.java
+++ Pavetheway/src/main/java/com/pavetheway/myapp/cart/dto/CartDto.java
@@ -1,5 +1,34 @@
 package com.pavetheway.myapp.cart.dto;
 
 public class CartDto {
-
+	//필드
+	private int num;
+	private int code;
+	private String id;
+	public CartDto(int num, int code, String id) {
+		super();
+		this.num = num;
+		this.code = code;
+		this.id = id;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	
 }
