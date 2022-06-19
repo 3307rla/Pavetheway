4b75fad2e9dec07a8188b3f112078e452c1ddd80
CoffeeIsLike <sfireal2030@gmail.com>
2022-06-20 08:57:17 +0900

    inquiryController updated

--- Pavetheway/src/main/java/com/pavetheway/myapp/inquiry/controller/InquiryController.java
+++ Pavetheway/src/main/java/com/pavetheway/myapp/inquiry/controller/InquiryController.java
@@ -1,5 +1,8 @@
 package com.pavetheway.myapp.inquiry.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -7,8 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.pavetheway.myapp.inquiry.dto.InquiryCommentDto;
 import com.pavetheway.myapp.inquiry.dto.InquiryDto;
 import com.pavetheway.myapp.inquiry.service.InquiryService;
 
@@ -56,4 +61,45 @@ public class InquiryController {
 		
 		return new ModelAndView("redirect:/inquiry/list.do");
 	}
+	
+	//새로운 댓글 저장 요청 처리
+	@RequestMapping("/inquiry/comment_insert")
+	public ModelAndView authCommentInsert(HttpServletRequest request, 
+			@RequestParam int ref_group) {
+		
+		service.saveComment(request);
+	
+		return new ModelAndView("redirect:/inquiry/detail.do?num="+ref_group);
+	}
+	
+	//댓글 더보기 요청 처리
+	@RequestMapping("/inquiry/ajax_comment_list")
+	public String ajaxCommentList(HttpServletRequest request) {
+		
+		service.moreCommentList(request);
+		
+		return "inquiry/ajax_comment_list";
+	}
+	//댓글 삭제 요청 처리
+	@RequestMapping("/inquiry/comment_delete")
+	@ResponseBody
+	public Map<String, Object> authCommentDelete(HttpServletRequest request) {
+		service.deleteComment(request);
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("isSuccess", true);
+		// {"isSuccess":true} 형식의 JSON 문자열이 응답되도록 한다. 
+		return map;
+	}
+	
+	//댓글 수정 요청처리 (JSON 을 응답하도록 한다)
+	@RequestMapping("/inquiry/comment_update")
+	@ResponseBody
+	public Map<String, Object> authCommentUpdate(InquiryCommentDto dto, HttpServletRequest request){
+		service.updateComment(dto);
+		Map<String, Object> map=new HashMap<String, Object>();
+		map.put("isSuccess", true);
+		// {"isSuccess":true} 형식의 JSON 문자열이 응답되도록 한다. 
+		return map;
+	}	
+	
 }
